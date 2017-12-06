7e41c236c6533496d4a93b32a58087dd46dd4714
suzumiya <jrgwyz@163.com>
2017-12-06 18:56:10 +0800

    ignore

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ song = AudioSegment.from_wav('example.wav')
 
 def trans_to_mp3():
     tags = {
-        'artist': '汪国平',
+        'artist': '陈向群',
         'album': 'EECS',
     }
-    song.export('example.mp3', format='mp3', bitrate='192k', tags=tags, cover='example.jpg')
+    song.export('20171202 操作系统 01.mp3', format='mp3', bitrate='192k', tags=tags, cover='example.jpg')
 
 
 def main():
