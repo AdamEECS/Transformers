178c941252fea9274a1686ba1fe9a25065d4553c
suzumiya <jrgwyz@163.com>
2018-01-22 17:23:28 +0800

    32

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from pydub import AudioSegment
 
 
-song = AudioSegment.from_wav('REC031.wav')
+song = AudioSegment.from_wav('REC032.wav')
 
 
 def trans_to_mp3():
     tags = {
-        'artist': '陈向群',
+        'artist': '李胜',
         'album': 'EECS',
     }
-    song.export('20180113 操作系统 下午 05.mp3', format='mp3', bitrate='192k', tags=tags, cover='example.jpg')
+    song.export('20180114 图形学 06.mp3', format='mp3', bitrate='192k', tags=tags, cover='example.jpg')
 
 
 def main():
